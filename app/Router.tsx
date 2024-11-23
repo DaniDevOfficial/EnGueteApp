5d2ef337ed2eea6d77a4f630334e2419730af120
DavDevDav <bischof.david.db@gmail.com>
2024-11-23 13:26:07 +0100

    initial

---
+++ app/Router.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import { Home } from './screens/Home';
+
+const Stack = createStackNavigator();
+
+export function Router() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
---
+++ app/screens/Home.tsx
@@ -0,0 +1,17 @@
+import { Box, Button, Text } from 'native-base'
+import React from 'react'
+
+export function Home({navigation }: any) {
+
+    return (
+        <Box flex={1} justifyContent="center" alignItems="center" p={4} >
+            <Text fontSize="2xl" mb={4}>Welcome to EnGuete!</Text>
+            <Button onPress={() => navigation.navigate('login')} my={2}>
+                <Text>Login</Text>
+            </Button>
+            <Button onPress={() => navigation.navigate('Sign up')} my={2}>
+                <Text>Sing up</Text>
+            </Button>
+        </Box>
+    )
+}
