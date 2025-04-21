17006da1642073e41999d663766b83378dac25f0
David <bischof.david.db@gmail.com>
2025-04-21 13:47:31 +0200

    11 - textinput readonly

--- app/components/settings/TextUpdate.tsx
+++ app/components/settings/TextUpdate.tsx
@@ -8,10 +8,11 @@ import {useText} from "../../utility/TextKeys/TextKeys";
 interface TextUpdateProps {
     title: string;
     text: string;
+    readonly?: boolean;
     onSuccess: (text: string) => Promise<void>;
 }
 
-export function TextUpdate({title, text,  onSuccess}: TextUpdateProps) {
+export function TextUpdate({title, text,  onSuccess, readonly = false}: TextUpdateProps) {
     const [isModalVisible, setModalVisible] = useState(false);
     const [value, setValue] = useState<string>(text);
     const [isSaving, setIsSaving] = useState(false);
@@ -33,9 +34,12 @@ export function TextUpdate({title, text,  onSuccess}: TextUpdateProps) {
                 <Text fontSize="xl" fontWeight="bold">
                     {text}
                 </Text>
-                <TouchableOpacity onPress={() => setModalVisible(true)}>
-                    <Icon as={Ionicons} name="create-outline" size={6} color="black"/>
-                </TouchableOpacity>
+
+                {!readonly && (
+                    <TouchableOpacity onPress={() => setModalVisible(true)}>
+                        <Icon as={Ionicons} name="create-outline" size={6} color="black" />
+                    </TouchableOpacity>
+                )}
             </HStack>
 
             <Modal isOpen={isModalVisible} onClose={() => setModalVisible(false)}>
--- app/screens/GroupSettings.tsx
+++ app/screens/GroupSettings.tsx
@@ -5,20 +5,20 @@ import {useUser} from "../context/userContext";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import {TouchableOpacity} from "react-native";
 import {TextUpdate} from "../components/settings/TextUpdate";
-import {useText} from "../utility/TextKeys/TextKeys";
+import {useText, useTexts} from "../utility/TextKeys/TextKeys";
 import {DangerZone} from "../components/settings/DangerZone";
 import {useGroup} from "../context/groupContext";
 import {UpdateGroupName, UpdateGroupNameType} from "../repo/Group";
+import {PERMISSIONS} from "../utility/Roles";
 
 
 export function GroupSettings() {
     const user = useUser();
     const [imageSrc, setImageSrc] = useState(user.user.profilePicture || 'https://imebehavioralhealth.com/wp-content/uploads/2021/10/user-icon-placeholder-1.png');
     const group = useGroup();
-    group.group.groupId
+    const text = useTexts(['updateGroupName']);
 
-        function handleEditImage() {
-    }
+
 
     async function handleEditGroupName(newGroupName: string) {
         const params: UpdateGroupNameType = {
@@ -43,34 +43,9 @@ export function GroupSettings() {
             <ScrollView>
                 <VStack maxH={'100%'} flex={1} alignItems="center" p={"10px 5px"} space={4}>
 
-                    <Box position="relative" width="70px" height="70px">
-                        <Image
-                            source={{uri: imageSrc}}
-                            alt="Profile picture"
-                            onError={() =>
-                                setImageSrc(
-                                    "https://imebehavioralhealth.com/wp-content/uploads/2021/10/user-icon-placeholder-1.png"
-                                )
-                            }
-                            width="70px"
-                            height="70px"
-                            borderRadius="full"
-                        />
-
-                        <TouchableOpacity onPress={handleEditImage}>
-                            <Icon
-                                as={Ionicons}
-                                name="create-outline"
-                                size={5}
-                                color="black"
-                                position="absolute"
-                                bottom={0}
-                                right={-15}
-                            />
-                        </TouchableOpacity>
-                    </Box>
-                    <TextUpdate text={group.group.groupName} title={useText('updateGroupName')}
-                                onSuccess={handleEditGroupName}/>
+                    <TextUpdate text={group.group.groupName} title={text.groupName}
+                                onSuccess={handleEditGroupName}
+                                readonly={group.group.userRoleRights.includes(PERMISSIONS.CAN_UPDATE_GROUP)}/>
 
                     <DangerZone/>
                 </VStack>
