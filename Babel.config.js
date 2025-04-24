19686cd051fee2c5ea3fb60b7ec1e60baf447eb8
David <bischof.david.db@gmail.com>
2025-04-24 20:04:38 +0200

    16 - eas setup

--- Babel.config.js
+++ Babel.config.js
@@ -4,6 +4,8 @@ module.exports = {
         ['module:react-native-dotenv', {
             moduleName: '@env',
             path: '.env',
-        }],
-    ],
+            allowUndefined: true,
+            safe: false
+        }]
+    ]
 };
