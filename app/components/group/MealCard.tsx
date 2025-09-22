6db427d20d051c95d804edf4c22e5dfe761e0ebe
davidMac <bischof.david.db@gmail.com>
2025-09-22 21:17:24 +0200

    26 - meal cards way prettier

--- app/components/group/MealCard.tsx
+++ app/components/group/MealCard.tsx
@@ -5,9 +5,10 @@ import {MealCard as MealCardType} from "../../repo/Group";
 import {getFancyTimeDisplay, getTime, semiNormalDateTime, shortDate} from "../../utility/Dates";
 import {getParticipantsText} from "../../utility/TextGeneration";
 import {PillTag} from "../UI/Pilltag";
-import {mealPreferenceText} from "../../utility/TextKeys/TextKeys";
+import {mealPreferenceText, useTexts} from "../../utility/TextKeys/TextKeys";
 import {MaterialIcons} from "@expo/vector-icons";
 import Ionicons from "react-native-vector-icons/Ionicons";
+import {ProfilePictureList} from "../UI/ProfilePictureList";
 
 type MealCardProps = {
     meal: MealCardType;
@@ -22,6 +23,14 @@ export function MealCard({meal}: MealCardProps) {
         navigation.navigate('meal', {mealId: meal.mealId});
     }
 
+    const images = [];
+
+    for (let i = 0; i < meal.participantCount; i++) {
+        images.push('https://i0.wp.com/picjumbo.com/wp-content/uploads/palm-tree-and-sky-in-early-evening-free-image.jpg?w=600&quality=80')
+    }
+
+    const textsForMeal = useTexts(['open', 'closed', 'finished'])
+
     const whenDate = shortDate(meal.dateTime);
     const whenTimeDisplay = getTime(meal.dateTime);
     const participantsText = getParticipantsText(meal.participantCount)
@@ -30,7 +39,7 @@ export function MealCard({meal}: MealCardProps) {
 
             <Pressable onPress={handleNavigate}>
                 <Box alignItems="center" p="4" borderRadius="md" shadow={'5'}
-                     backgroundColor={meal.closed ? 'coolGray.200' : 'white'} width={'95%'} my={2}
+                     backgroundColor={meal.closed ? 'coolGray.300' : 'coolGray.200'} width={'95%'} my={2}
                      position={'relative'}>
                     {meal.fulfilled && (
                         <Box position="absolute" top={-11} right={-10}>
@@ -42,39 +51,68 @@ export function MealCard({meal}: MealCardProps) {
                             />
                         </Box>
                     )}
-                    <Flex
+                    <HStack
                         justifyContent={"space-between"}
                         flexDir={'row'}
                         width={"100%"}
+                        space={'4'}
                     >
-                        <HStack
-                            w='50%'
-                            flexDir={'column'}
-
+                        <VStack
+                            w='45%'
+                            space={'5'}
                         >
-                            <Text isTruncated fontSize={'xl'} fontWeight={'bold'}>
-                                {meal.title}
-                            </Text>
-                            <HStack space={2}>
-                                <Text isTruncated>{whenDate}</Text>
-                                <Text>|</Text>
-                                <Text isTruncated>{whenTimeDisplay}</Text>s
-                            </HStack>
-                        </HStack>
-                        <Flex alignItems={'flex-end'} w={'50%'}>
-                            <Text isTruncated>
-                                {participantsText}
-                            </Text>
+                            <VStack>
+                                <Text isTruncated fontSize={'xl'} fontWeight={'bold'}>
+
+                                    {meal.title}
+                                </Text>
+                                <HStack space={2}>
+                                    <Text isTruncated>{whenDate}</Text>
+                                    <Text>{'|'}</Text>
+                                    <Text isTruncated>{whenTimeDisplay}</Text>s
+                                </HStack>
+                            </VStack>
                             <Flex flexDir={'row'}>
                                 {meal.isCook && <PillTag text={'‍👨‍🍳'}/>}
                                 <PillTag text={mealPreferenceText(meal.userPreference)} colorScheme={'orange'}/>
                             </Flex>
+                        </VStack>
+                        <Flex alignItems={'flex-end'} justifyContent={'space-between'} w={'45%'}>
+
+                            <PillTagBasedOnMealOpenAndFinished meal={meal} textKeys={textsForMeal}/>
+                            <ProfilePictureList
+                                profilePictures={images}
+                                totalAmount={meal.participantCount}/>
+
                         </Flex>
-                    </Flex>
+                    </HStack>
                 </Box>
 
             </Pressable>
 
         </>
     );
+}
+
+
+interface PillTagBasedOnMealOpenAndFinishedProps {
+    meal: MealCardType
+    textKeys: { closed: string; finished: string; open: string }
+}
+
+function PillTagBasedOnMealOpenAndFinished({meal, textKeys}: PillTagBasedOnMealOpenAndFinishedProps) {
+
+    if (meal.closed) {
+        return (<PillTag text={textKeys.closed} colorScheme={'blueGray'}/>)
+    }
+
+    if (meal.fulfilled) {
+        return (
+            <PillTag text={textKeys.finished} colorScheme={'orange'}/>
+        )
+    }
+
+    return (
+        <PillTag text={textKeys.open} colorScheme={'green'}/>
+    )
 }
